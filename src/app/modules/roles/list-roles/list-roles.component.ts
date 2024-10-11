e1173d69751319b6d9da56446e285fdc2cc7b466
hijujuy <dmchumacero@gmail.com>
2024-10-11 09:50:05 -0300

    crud rolespermissions

--- src/app/modules/roles/list-roles/list-roles.component.ts
+++ src/app/modules/roles/list-roles/list-roles.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateRolesComponent } from '../create-roles/create-roles.component';
+import { RolesService } from '../service/roles.service';
+import { EditRolesComponent } from '../edit-roles/edit-roles.component';
+import { DeleteRolesComponent } from '../delete-roles/delete-roles.component';
 
 @Component({
   selector: 'app-list-roles',
@@ -9,19 +12,79 @@ import { CreateRolesComponent } from '../create-roles/create-roles.component';
 })
 export class ListRolesComponent { 
 
+  search:string = '';
+  ROLES:any;
+  isLoading$:any;
+
+  totalPages:number = 0;
+  currentPage:number = 0;
+
   constructor(
     public modalService: NgbModal,
+    public rolesService: RolesService,
   ) {
 
   }
 
   ngOnInit(): void {
     
+    this.isLoading$ = this.rolesService.isLoading$;
+
+    this.listRoles();
     
+  }
+
+  listRoles(page = 1) {
+
+    this.rolesService.listRoles(page, this.search).subscribe((resp: any) => {
+      console.log(resp);
+      this.ROLES = resp.roles;
+      this.totalPages = resp.total;
+      this.currentPage = page;
+    });
+
+  }
+
+  loadPage($event:any) {
+    this.listRoles($event);
   }
 
   createRol() {
     const modalRef = this.modalService.open(CreateRolesComponent,{centered: true, size: 'md'});
+
+    modalRef.componentInstance.RoleC.subscribe((role:any) => {
+      this.ROLES.push(role);
+    });
+  }
+
+  editRole(ROL:any) {
+    const modalRef = this.modalService.open(EditRolesComponent,{centered: true, size: 'md'});
+
+    modalRef.componentInstance.ROLE_SELECTED = ROL;
+
+    modalRef.componentInstance.RoleE.subscribe((role:any) => {
+      let INDEX = this.ROLES.findIndex((rol:any) => rol.id == ROL.id);
+      if(INDEX != 1) {
+        this.ROLES[INDEX] = role;
+      }
+      
+      /* this.ROLES.push(role); */
+    });
+  }
+
+  deleteRole(ROL:any) {
+    const modalRef = this.modalService.open(DeleteRolesComponent,{centered: true, size: 'md'});
+
+    modalRef.componentInstance.ROLE_SELECTED = ROL;
+
+    modalRef.componentInstance.RoleD.subscribe((role:any) => {
+      let INDEX = this.ROLES.findIndex((rol:any) => rol.id == ROL.id);
+      if(INDEX != 1) {
+        this.ROLES.splice(INDEX, 1)
+      }
+      
+      /* this.ROLES.push(role); */
+    });
   }
 
 }
---
+++ src/app/modules/roles/service/roles.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, finalize, Observable } from 'rxjs';
+import { AuthService } from '../../auth';
+import { URL_SERVICIOS } from 'src/app/config/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RolesService {
+
+  isLoading$: Observable<boolean>;
+  isLoadingSubject: BehaviorSubject<boolean>;
+  
+  constructor(
+    private http: HttpClient,
+    public authservice: AuthService,
+  ) {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
+  }
+
+  listRoles(page = 1, search:string = '') {
+    this.isLoadingSubject.next(true);
+
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authservice.token});
+
+    let URL = URL_SERVICIOS + "/roles?page=" + page + "&search=" + search;
+
+    return this.http.get(URL, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    );
+  }
+
+  registerRole(data:any) {
+    this.isLoadingSubject.next(true);
+
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authservice.token});
+
+    let URL = URL_SERVICIOS + "/roles";
+
+    return this.http.post(URL, data, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    );
+  }
+
+  updateRole(ID_ROLE:string, data:any) {
+    this.isLoadingSubject.next(true);
+
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authservice.token});
+
+    let URL = URL_SERVICIOS + "/roles/" + ID_ROLE;
+
+    return this.http.put(URL, data, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    );
+  }
+
+  deleteRole(ID_ROLE:string) {
+    this.isLoadingSubject.next(true);
+
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authservice.token});
+
+    let URL = URL_SERVICIOS + "/roles/" + ID_ROLE;
+
+    return this.http.delete(URL, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    );
+  }
+
+}
