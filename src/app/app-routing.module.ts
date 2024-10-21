4b87e9d1025d5f8b41174f0c38948f19a04d49a0
Alfredo <dominiolibre@gmail.com>
2024-10-21 13:44:49 -0300

    front-v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,8 @@ export const routes: Routes = [
 
 @NgModule({
   //imports: [RouterModule.forRoot(routes)],
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  //imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes,)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
